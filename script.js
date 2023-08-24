2a7f10019c3575cb4a7bf81b9f12a757ff545738
Jacob <jakevilevac@gmail.com>
2023-08-23 20:31:59 -0600

    switched to <canvas>

--- script.js
+++ script.js
@@ -1,22 +1,66 @@
-// script.js
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-const data = {
-  // your given data structure here
-};
+let data = {};
 
-const container = document.getElementById("grid-container");
-const quests = data.quests;
+function createDataPoint(x = 0, y = 0, size = 10) {
+    const id = Math.random().toString(36).substr(2, 9);  // Generate a random ID for each data point
+    data[id] = { x, y, size };
+}
 
-quests.forEach(quest => {
-  const questElement = document.createElement("div");
-  questElement.className = "quest";
+// Example usage
+createDataPoint(100, 100, 15);
+createDataPoint(300, 200, 20);
 
-  // Calculate position based on x and y values
-  const x = (quest.x + 0.5) * container.offsetWidth;
-  const y = (quest.y + 0.5) * container.offsetHeight;
+let offsetX = 0;
+let offsetY = 0;
+let isDragging = false;
+let lastX = 0;
+let lastY = 0;
+let zoom = 1;
 
-  questElement.style.left = `${x}px`;
-  questElement.style.top = `${y}px`;
-
-  container.appendChild(questElement);
+canvas.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    lastX = e.clientX;
+    lastY = e.clientY;
 });
+
+canvas.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        offsetX += e.clientX - lastX;
+        offsetY += e.clientY - lastY;
+        lastX = e.clientX;
+        lastY = e.clientY;
+        draw();
+    }
+});
+
+canvas.addEventListener('wheel', (e) => {
+    e.preventDefault();
+    if (e.deltaY > 0) {
+        zoom *= 1.1;
+    } else {
+        zoom /= 1.1;
+    }
+    draw();
+});
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.save();
+    ctx.translate(offsetX, offsetY);
+    ctx.scale(zoom, zoom);
+    for (let id in data) {
+        const point = data[id];
+        ctx.beginPath();
+        ctx.arc(point.x, point.y, point.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+    ctx.restore();
+}
+
+draw();
